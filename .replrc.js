a6bc6b0d574776692d2b59805236a18db693c9c8
dany1468 <dany1468@gmail.com>
2018-06-23 09:57:45 +0900

    Prepare repl

---
+++ .replrc.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const github_token = process.env.GITHUB_TOKEN;
+const octokit = require('@octokit/rest')();
+
+octokit.authenticate({
+    type: 'token',
+    token: github_token
+});
+
+module.exports = {
+  enableAwait: true
+}
+
+module.exports = {context: {octokit:octokit}};
